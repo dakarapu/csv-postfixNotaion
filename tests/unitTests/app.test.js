ad51ff796c9fce2d3a76905a9180bf91fd87d401
Ravikanth Dakarapu <ravikanth.dakarapu@gmail.com>
2019-05-05 02:23:34 +1000

    updated tests for coverage

--- tests/unitTests/app.test.js
+++ tests/unitTests/app.test.js
@@ -1,44 +1,28 @@
-//jest.mock("../../src/app"); // this happens automatically with automocking
-//jest.mock("../../src/helpers/mapper");
-//jest.mock("../../src/helpers/files");
-
 import "@babel/polyfill";
-
 import app from "../../src/app";
-//import { mapping, trimExpressions } from "../../src/helpers/mapper";
-import { readFile, writeFile } from "../../src/helpers/files";
 
-beforeEach(() => {
+beforeAll(() => {
   process.argv = ["", "", "input.csv", "output.csv"];
 });
 
-describe("app - Unit Test", () => {
-  // it("works with async/await", async () => {
-  //   expect.assertions(1);
-  //   const data = await readFile("input.csv");
-  //   expect(data).toBeInstanceOf(Buffer);
-  // });
+afterEach(() => {
+  process.argv = [];
+});
 
-  it("successful app test", async () => {
+describe("App - Unit Test - 1", () => {
+  it("Testing successful path", async () => {
     await app();
-    // try {
-    //   const readData = await readFile(__dirname + "/helpers/input.csv");
-    //   const list = await trimExpressions(readData);
-    //   const mappedList = await mapping(list);
-    //   let writeData = "";
-    //   //console.log("mappedList", mappedList);
-    //   mappedList.forEach(async element => {
-    //     writeData += element + "\r\n";
-    //   });
-    //   let result = await writeFile(
-    //     __dirname + "/helpers/output.csv",
-    //     writeData
-    //   );
-    //   console.table(result);
-    //   //return result;
-    // } catch (e) {
-    //   console.error("Received Error response:", e);
-    //   process.exit(1);
-    // }
+  });
+});
+
+describe("App - Unit Test - 2", () => {
+  it("Testing error catch with process exit", async () => {
+    const mockExit = jest.spyOn(process, "exit").mockImplementation(() => {});
+    try {
+      await app();
+    } catch (e) {
+      expect(e).toThrow();
+      expect(mockExit).toHaveBeenCalled();
+    }
   });
 });
